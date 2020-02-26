7b6be7dca0f1822fd7ab9ff6d9c94715d6f4be43
StanislavJochman <s.jochman@gmail.com>
2020-02-26 16:44:11 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@ const second = 1000,
       minute = second * 60,
       hour = minute * 60,
       day = hour * 24;
-let countDown0 = new Date('Feb 10, 2020 18:50:00').getTime();
-let countDown1 = new Date('Feb 10, 2020 18:51:30').getTime();
-let countDown2 = new Date('Feb 10, 2020 18:51:40').getTime();
-let countDown3 = new Date('Feb 10, 2020 18:51:50').getTime();
-let countDown4 = new Date('Feb 10, 2020 18:52:00').getTime();
-let countDown5 = new Date('Feb 10, 2020 18:52:10').getTime();
-let countDown6 = new Date('Feb 10, 2020 18:52:20').getTime();
+let countDown0 = new Date('Feb 26, 2020 15:00:00').getTime();
+let countDown1 = new Date('Feb 26, 2020 15:10:00').getTime();
+let countDown2 = new Date('Feb 26, 2020 15:40:00').getTime();
+let countDown3 = new Date('Feb 26, 2020 16:00:00').getTime();
+let countDown4 = new Date('Feb 26, 2020 16:30:00').getTime();
+let countDown5 = new Date('Feb 26, 2020 17:00:00').getTime();
+let countDown6 = new Date('Feb 26, 2020 17:30:00').getTime();
 
 x = setInterval(function() {
   let now = new Date().getTime();
   document.getElementById('curr_time').innerText = new Date().toLocaleTimeString('en-GB');
-  if (countDown1 - now < 0) {
+  if (countDown0 - now < 0) {
     $('.list-hours li').eq(0).addClass('today');
   }
   if (countDown1 - now < 0) {
