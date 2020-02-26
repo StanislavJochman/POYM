1c08dd21d8959e4968cb6557956d53ba15eeed91
StanislavJochman <s.jochman@gmail.com>
2020-02-26 19:31:37 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@ const second = 1000,
       minute = second * 60,
       hour = minute * 60,
       day = hour * 24;
-let countDown0 = new Date('Feb 26, 2020 15:00:00').getTime();
-let countDown1 = new Date('Feb 26, 2020 15:10:00').getTime();
-let countDown2 = new Date('Feb 26, 2020 15:40:00').getTime();
-let countDown3 = new Date('Feb 26, 2020 16:00:00').getTime();
-let countDown4 = new Date('Feb 26, 2020 16:30:00').getTime();
-let countDown5 = new Date('Feb 26, 2020 17:00:00').getTime();
-let countDown6 = new Date('Feb 26, 2020 17:30:00').getTime();
+let countDown0 = new Date('Feb 27, 2020 15:00:00').getTime();
+let countDown1 = new Date('Feb 27, 2020 15:10:00').getTime();
+let countDown2 = new Date('Feb 27, 2020 15:40:00').getTime();
+let countDown3 = new Date('Feb 27, 2020 16:00:00').getTime();
+let countDown4 = new Date('Feb 27, 2020 16:30:00').getTime();
+let countDown5 = new Date('Feb 27, 2020 17:00:00').getTime();
+let countDown6 = new Date('Feb 27, 2020 17:30:00').getTime();
+let countDown7 = new Date('Feb 27, 2020 18:00:00').getTime();
+let countDown8 = new Date('Feb 27, 2020 19:45:00').getTime();
+let countDown9 = new Date('Feb 27, 2020 19:50:00').getTime();
 
 x = setInterval(function() {
   let now = new Date().getTime();
@@ -54,6 +57,39 @@ x = setInterval(function() {
     $('.list-hours li').eq(3).removeClass('today');
     $('.list-hours li').eq(4).removeClass('today');
     $('.list-hours li').eq(5).removeClass('today');
+  }
+  if (countDown7 - now < 0) {
+    $('.list-hours li').eq(7).addClass('today');
+    $('.list-hours li').eq(0).removeClass('today');
+    $('.list-hours li').eq(1).removeClass('today');
+    $('.list-hours li').eq(2).removeClass('today');
+    $('.list-hours li').eq(3).removeClass('today');
+    $('.list-hours li').eq(4).removeClass('today');
+    $('.list-hours li').eq(5).removeClass('today');
+    $('.list-hours li').eq(6).removeClass('today');
+  }
+  if (countDown8 - now < 0) {
+    $('.list-hours li').eq(8).addClass('today');
+    $('.list-hours li').eq(0).removeClass('today');
+    $('.list-hours li').eq(1).removeClass('today');
+    $('.list-hours li').eq(2).removeClass('today');
+    $('.list-hours li').eq(3).removeClass('today');
+    $('.list-hours li').eq(4).removeClass('today');
+    $('.list-hours li').eq(5).removeClass('today');
+    $('.list-hours li').eq(7).removeClass('today');
+    $('.list-hours li').eq(6).removeClass('today');
+  }
+  if (countDown9 - now < 0) {
+    $('.list-hours li').eq(9).addClass('today');
+    $('.list-hours li').eq(0).removeClass('today');
+    $('.list-hours li').eq(1).removeClass('today');
+    $('.list-hours li').eq(2).removeClass('today');
+    $('.list-hours li').eq(3).removeClass('today');
+    $('.list-hours li').eq(4).removeClass('today');
+    $('.list-hours li').eq(5).removeClass('today');
+    $('.list-hours li').eq(6).removeClass('today');
+    $('.list-hours li').eq(7).removeClass('today');
+    $('.list-hours li').eq(8).removeClass('today');
   }
 
 
